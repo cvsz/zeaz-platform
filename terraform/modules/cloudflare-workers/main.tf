233adad456ff5ea80ec57d6ff49c40e9c86aa78e
cvsz <seaza@msn.com>
2026-05-13 02:58:30 +0700

    Fix workers script resource name argument

--- terraform/modules/cloudflare-workers/main.tf
+++ terraform/modules/cloudflare-workers/main.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_workers_script" "this" {
   account_id = var.account_id
-  script_name = coalesce(var.name, "default-worker")
+  name       = coalesce(var.name, "default-worker")
   content    = "addEventListener('fetch', event => event.respondWith(new Response('ok')))"
 }
